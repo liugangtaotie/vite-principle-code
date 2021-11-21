28545bf625cdc31a26e6cf009b656c51d01d49ee
liugang <lgcf1314@gmail.com>
2021-11-21 20:24:43 +0800

    FIX:211121提交v1.3

--- index.js
+++ index.js
@@ -13,20 +13,53 @@ app.use(async (ctx) => {
   // '/' => "index.html"
   if (url === '/') {
     ctx.type = 'text/html'
-    const content = fs.readFileSync('index.html', 'utf-8')
-
+    let content = fs.readFileSync('index.html', 'utf-8')
+    content = content.replace(
+      '<script',
+      ` <script>
+    window.process = {
+      env: {
+        ENV_NODE: 'dev'
+      }
+    }
+  </script>
+  <script`
+    )
     ctx.body = content
   }
 
   // '/src/main.js' => 'src/main.js'
   else if (url.endsWith('.js')) {
     ctx.type = 'application/javascript'
-
     const p = path.resolve(__dirname, url.slice(1))
-
     const content = fs.readFileSync(p, 'utf-8')
+    ctx.body = rewriteImport(content)
+  }
 
-    ctx.body = content
+  //
+  else if (url.startsWith('/@modules/')) {
+    const prefix = path.resolve(
+      __dirname,
+      'node_modules',
+      url.replace('/@modules/', '')
+    )
+
+    const module = require(prefix + '/package.json').module
+    const p = path.resolve(prefix, module)
+    const content = fs.readFileSync(p, 'utf-8')
+    ctx.type = 'application/javascript'
+    ctx.body = rewriteImport(content)
+  }
+
+  function rewriteImport(content) {
+    console.info(content)
+    return content.replace(/ from ['|"]([^'"]+)['|"]/g, (s0, s1) => {
+      if (s1[0] !== '.' && s1[0] !== '/') {
+        return ` from '/@modules/${s1}'`
+      } else {
+        return s0
+      }
+    })
   }
 })
 
--- src/moduleA.js
+++ src/moduleA.js
@@ -1,1 +1,11 @@
 export const str = 'hello world'
+
+import { createApp, h } from 'vue'
+
+const App = {
+  render() {
+    return h('div', 'hello world')
+  },
+}
+
+createApp(App).mount('#app')
